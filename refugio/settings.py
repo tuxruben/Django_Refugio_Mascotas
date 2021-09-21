689d9954e8f39cdeab60601d90b3c4a3f7258062
tuxruben <49528908+tuxruben@users.noreply.github.com>
2021-09-21 11:56:29 -0500

    Update settings.py

--- refugio/settings.py
+++ refugio/settings.py
@@ -138,5 +138,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rubenoctavio@itsn.edu.mx'
-EMAIL_HOST_PASSWORD = '44984498Ruben_5'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
